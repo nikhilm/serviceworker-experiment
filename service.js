31ef7777a3ec69a41c9c0e273b77a7da934f1d7e
Nikhil Marathe (On vacation & offline July 27 - Aug 18) <nsm.nikhil@gmail.com>
2014-07-25 15:27:29 -0700

    Update service.js to slightly newer API

--- service.js
+++ service.js
@@ -3,14 +3,12 @@ onfetch = function(e) {
   var request = e.request;
   dump("request.url = " + request.url + "\n");
   dump("request.method = " + request.method + "\n");
-  dump("request.origin = " + request.origin + "\n");
-  dump("request.referrer = " + request.referrer + "\n");
   dump("request.body = " + request.body + "\n");
 
   var url = new URL(request.url);
   dump("PATH " + url.pathname + "\n");
   if (url.pathname == "/fakescript.js") {
-    e.respondWith(new SameOriginResponse({body: "alert('annoying popup');"}));
+    e.respondWith(new Response({body: "alert('annoying popup');"}));
     return;
   }
 }
