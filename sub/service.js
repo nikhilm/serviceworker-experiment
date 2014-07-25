3b924df3d637ee1a15aa081689e5aa103e5971be
Nikhil Marathe (On vacation & offline July 27 - Aug 18) <nsm.nikhil@gmail.com>
2014-07-25 15:48:21 -0700

    Update service.js to slightly newer API

--- sub/service.js
+++ sub/service.js
@@ -3,14 +3,12 @@ onfetch = function(e) {
   var request = e.request;
   dump("request.url = " + request.url + "\n");
   dump("request.method = " + request.method + "\n");
-  dump("request.origin = " + request.origin + "\n");
-  dump("request.referrer = " + request.referrer + "\n");
   dump("request.body = " + request.body + "\n");
 
   var url = new URL(request.url);
   dump("PATH " + url.pathname + "\n");
-  if (url.pathname == "/sub/index.html") {
-    e.respondWith(new SameOriginResponse({body: "Fake embedded content"}));
+  if (url.pathname == "/serviceworker-experiment/sub/index.html") {
+    e.respondWith(new Response({body: "Fake embedded content"}));
     return;
   }
 }
